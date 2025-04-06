4f4a8115a666934935e6880d6046b66d1879aad6
ZiarZer <dev@ziarzer.fr>
2025-04-06 22:49:16 +0200

    feat: init app with a color selector

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import ColorSelector from './components/ColorSelector.vue'
+
+const hexColor = ref('#ffffff')
+</script>
+
+<template>
+  <ColorSelector :disabled="false" v-model="hexColor" />
+</template>
+
+<style scoped></style>
---
+++ src/components/ColorSelector.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  disabled: Boolean,
+})
+
+const hexColor = defineModel()
+</script>
+
+<template>
+  <div class="selector-wrapper">
+    <input type="color" v-model="hexColor" :disabled="disabled" />
+    <input type="text" id="hex-input-text" v-model="hexColor" :disabled="disabled" />
+  </div>
+</template>
+
+<style scoped>
+.selector-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.125em;
+}
+
+input {
+  width: 4.5em;
+}
+
+#hex-input-text {
+  border: solid white 0.25px;
+  color: white;
+  background-color: black;
+}
+</style>
