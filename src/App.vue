5fd9a4e40b15d63ea855bf3b2170c21ccae1f836
ZiarZer <dev@ziarzer.fr>
2025-04-08 22:00:37 +0200

    feat: enable adding color tiles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup>
 import IntervalConverterTool from './components/IntervalConverterTool.vue';
 import ShadeComputerTool from './components/ShadeComputerTool.vue'
+import ColorTile from './components/ColorTile.vue'
+import { ref } from 'vue';
+
+const addTile = () => colors.value.push('#ffffff')
+
+const colors = ref([])
 </script>
 
 <template>
   <div id="tools">
     <ShadeComputerTool />
     <IntervalConverterTool />
+  </div>
+  <hr />
+  <button @click="addTile">Add tile</button>
+  <div id="tiles-container">
+    <ColorTile v-for="() in colors" />
   </div>
 </template>
 
@@ -29,7 +40,22 @@ import ShadeComputerTool from './components/ShadeComputerTool.vue'
 
 #tools {
   display: flex;
-  justify-content: space-between;
+  justify-content: end;
   gap: 1em;
+}
+
+#tiles-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column-reverse;
+  gap: 0.5em;
+}
+
+hr {
+  margin: 1em;
+}
+
+button {
+  margin: 1em;
 }
 </style>
