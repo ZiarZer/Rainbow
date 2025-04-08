0d46a2297465a3930751e6a0d79f05d021ccfa0e
ZiarZer <dev@ziarzer.fr>
2025-04-08 21:33:54 +0200

    feat: add ColorTile component

---
+++ src/components/ColorTile.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+import ColorSelector from './ColorSelector.vue';
+
+const colorHex = ref('#ffffff');
+</script>
+<template>
+  <div :style="{ backgroundColor: colorHex }" class="color-tile">
+    <ColorSelector :disabled="false" v-model="colorHex" />
+  </div>
+</template>
+<style scoped>
+.color-tile {
+  border-radius: 1em;
+  padding: 0.25em;
+}
+</style>
